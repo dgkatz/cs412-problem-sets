2405d00bbbcfa9ac74c40b9efa64966198886fbd
Dan Katz <dgkatz@gmail.com>
2020-10-21 23:09:23 -0400

    completed ps3

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Hello World!' });
+});
+
+router.post('/', function(req, res, next) {
+  req.body.title
+  res.render('index', { title: req.body.title, title_length: req.body.title.length });
+});
+
+module.exports = router;
