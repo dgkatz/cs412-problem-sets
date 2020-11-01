719203e93dc9aa931521d20894bc645e7970b567
Dan Katz <dgkatz@gmail.com>
2020-11-01 00:04:05 -0400

    finished ps4

---
+++ config/openweathermap.js
@@ -0,0 +1,7 @@
+const fetchOptions = {
+    method: 'GET',
+    url: 'https://api.openweathermap.org/data/2.5/weather',
+    secretKey: '45ea0187f4d01ad3a9d3b5a0caedc810'
+};
+
+module.exports = {fetchOptions}
