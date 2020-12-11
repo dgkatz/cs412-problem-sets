5a349ac899e9c4ee60cf97762a7a6cc6d5c473e9
Dan Katz <dgkatz@gmail.com>
2020-12-11 15:38:11 -0500

    finish assignment 5b

---
+++ routes/ps5b.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+const {URL, URLSearchParams} = require('url');
+const fetch = require('node-fetch');
+const redis = require('redis');
+const {fetchOptions} = require('../config/openweathermap')
+
+const client = redis.createClient();
+
+
+router.post('/', function (req, res, next) {
+    const city = req.body.city
+    client.get(`${city}`, function(err, reply) {
+        // reply is null when the key is missing
+        if (err) throw err;
+        if (reply) {
+            reply = JSON.parse(reply);
+            res.json({
+                data: reply,
+                cache_hit: true
+            });
+        }else {
+            let url = new URL(fetchOptions.url)
+            const params = new URLSearchParams();
+            params.append('q', req.body.city);
+            params.append('appid', fetchOptions.secretKey);
+            url.search = params.toString();
+            fetch(url, {method: fetchOptions.method})
+                .then(res => res.json())
+                .then(json => {
+                    const data = { city:  json.name, temp: json.main.temp}
+                    client.set(`${city}`, JSON.stringify(data), 'EX', 15)
+                    res.json({
+                        data: data,
+                        cache_hit: false
+                    });
+                })
+                .catch(err => console.error(err));
+        }
+    });
+});
+
+module.exports = router;
