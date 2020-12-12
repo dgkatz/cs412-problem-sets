bfd8b5fdaefe7c0f92d91d308e30346fb55b00d1
Dan Katz <dgkatz@gmail.com>
2020-12-11 21:46:57 -0500

    move PS4 code into backend directory

--- backend/config/openweathermap.js
+++ backend/config/openweathermap.js
@@ -1,7 +1,7 @@
 const fetchOptions = {
     method: 'GET',
     url: 'https://api.openweathermap.org/data/2.5/weather',
-    secretKey: '45ea0187f4d01ad3a9d3b5a0caedc810'
+    secretKey: '1881d6ab468d0cc54c4e793a5ea07e33'
 };
 
 module.exports = {fetchOptions}
